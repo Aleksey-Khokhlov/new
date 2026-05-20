6851ea417c61f06b7cb67b8136fae2fb34d8e616
Aleksey-Khokhlov <alex.swetwet@yandex.ru>
2026-05-20 13:50:08 +0300

    Update task_7.sql

--- task_7.sql
+++ task_7.sql
@@ -1,1 +1,22 @@
 /*добавьте сюда запросы для решения задания 6*/
+BEGIN;
+
+LOCK TABLE cafe.managers IN EXCLUSIVE MODE; -- Позволяет другим пользователям толь читать данные
+
+ALTER TABLE cafe.managers ADD COLUMN phone TEXT[];
+
+WITH new_numbers AS (
+    SELECT 
+        manager_uuid,
+        CONCAT('8-800-2500-', ROW_NUMBER() OVER (ORDER BY manager_name) + 99) AS new_phone,
+        manager_phone
+    FROM cafe.managers
+)
+UPDATE cafe.managers
+SET phone = ARRAY[nn.new_phone, nn.manager_phone]
+FROM new_numbers nn
+WHERE cafe.managers.manager_uuid = nn.manager_uuid;
+
+ALTER TABLE cafe.managers DROP COLUMN manager_phone;
+
+COMMIT;
